7a7688739e44f13c8894c2734474280f97729973
Angus-Paillaugue <angus.paillaugue40@gmail.com>
2025-11-06 10:13:18 +0100

    updates

--- shared.py
+++ shared.py
@@ -3,7 +3,7 @@ import os
 HERE = os.path.dirname(os.path.abspath(__file__))
 
 # --- START TYPE DETECTION PATHS ---
-TYPE_DETECTION_OUT_VERSION = "1.0.1"
+TYPE_DETECTION_OUT_VERSION = "1.0.0"
 TYPE_DETECTION_DIR = os.path.join(HERE, "type_detection")
 TYPE_DETECTION_OUT_DIR = os.path.join(TYPE_DETECTION_DIR, "out")
 TYPE_DETECTION_OUT_DATASET_DIR = os.path.join(TYPE_DETECTION_DIR, "fashion-dataset")
